46b88b222354ae13610fc73039867fad5c3c0f39
Kou Guandong <gdkou90@gmail.com>
2017-02-27 13:48:36 +0800

    fixing gulp prod config

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ const eslint = require('gulp-eslint');
 const cssnano = require('gulp-cssnano');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
-// const minifyHtml = require('gulp-htmlmin');
+const minifyHtml = require('gulp-htmlmin');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const browserSync = require('browser-sync').create();
@@ -15,14 +15,22 @@ const assetsInjector = require('gulp-assets-injector')();
 const sass = require('gulp-sass');
 const runSequence = require('run-sequence');
 const rev = require('gulp-rev');
+const streamqueue = require('streamqueue');
+var revCollector = require('gulp-rev-collector'); //- 路径替换
+var revReplace = require('gulp-rev-replace');
 
 // Config Environment
 const DIST = 'dist';
 const isProd = process.env.NODE_ENV === 'production';
 
-// delete 'dist' directory
-// gulp.task('clean', () => del(DIST));
+let serverPath = './src';
 
+if (isProd) {
+  serverPath = './dist'
+}
+
+// delete 'dist' directory
+gulp.task('clean', () => del(DIST));
 
 gulp.task('sass', () => {
   let stream = gulp.src(['src/scss/*.scss'])
@@ -40,31 +48,40 @@ gulp.task('sass', () => {
       .pipe(rev.manifest({
         merge: true
       }))
-      .pipe(gulp.dest('dist/rev/css'));
+      .pipe(gulp.dest('dist/rev/'));
   }
   return stream;
 });
 
 // inspect JS
-gulp.task('jshint', function () {
-  return gulp.src('src/js/*.js')
-  .pipe(babel({
-   presets: ['es2015']
-   }))
-   .pipe(gulp.dest('src/js'));
+gulp.task('jshint', () => {
+  let stream = gulp.src('src/js/*.js')
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(gulp.dest('src/js'));
+  if (isProd) {
+    stream.pipe(uglify())
+      .pipe(rev())
+      .pipe(gulp.dest('dist/js'))
+      .pipe(rev.manifest({
+        merge: true
+      }))
+      .pipe(gulp.dest('dist/rev/'));
+  }
 });
 
 // synchronize browser
-gulp.task('browserSync', function() {
+gulp.task('browserSync', function () {
   browserSync.init({
     server: {
-      baseDir: "./src/"
+      baseDir: serverPath
     }
   })
 });
 
 // Watch Files For Changes
-gulp.task('watch', ['browserSync', 'sass', 'jshint'], function() {
+gulp.task('watch', ['browserSync', 'sass', 'jshint'], function () {
   gulp.watch(['src/**/*.js']).on('change', browserSync.reload);
   gulp.watch(['src/**/*.html']).on('change', browserSync.reload);
   gulp.watch('src/scss/*.scss', ['sass']);
@@ -73,63 +90,37 @@ gulp.task('watch', ['browserSync', 'sass', 'jshint'], function() {
 // run dev build sequence
 gulp.task('dev', function (callback) {
   runSequence(
-    ['watch', 'sass', 'uglify', 'browserSync'],
+    ['watch', 'sass', 'jshint', 'browserSync'],
     callback
   )
 });
-gulp.task('default', function() {
+gulp.task('default', function () {
   runSequence('dev');
 });
 
-// uglify JS and pipe to dist
-gulp.task('uglify', function() {
-  return gulp.src('src/js/*.js')
-    .pipe(babel({
-      presets: ['es2015']
-    }))
-    // .pipe(revCollector({replaceReved: true}))
-    .pipe(uglify())
-    .pipe(rev())
-    .pipe(gulp.dest('dist/js'))
-    .pipe(rev.manifest({
-      merge: true
-    }))
-    .pipe(gulp.dest('dist/rev/js'));
-});
 
-// compile sass and pipe to dist
-gulp.task('sass-prod', function () {
-  return gulp.src('src/scss/*.scss')
-    .pipe(browserSync.reload({
-      stream: true
-    }))
-    // .pipe(revCollector({replaceReved: true}))
-    .pipe(sass({outputStyle: 'compressed'}))
-    .on('error', sass.logError)
-    .pipe(rev())
-    .pipe(gulp.dest('dist/css'))
-    .pipe(rev.manifest({
-      merge: true
-    }))
-    .pipe(gulp.dest('dist/rev/css'))
+gulp.task('browser-sync', ['watch'], () => {
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: serverPath,
+    },
+  });
 });
 
 // minify images
-gulp.task('images-prod', function () {
-  return streamqueue({objectMode: true},
-    gulp.src(['src/assets/**/*.+(png|jpg|jpeg|gif|svg)'])
-      .pipe(gulp.dest('dist/assets'))
-  )
+gulp.task('images', function () {
+  let stream = gulp.src(['src/assets/**/*.+(png|jpg|jpeg|gif|svg)'])
+    .pipe(gulp.dest('dist/assets'));
 });
 
 // copy html and common resources to dist
-gulp.task('copy', function () {
-  return streamqueue({objectMode: true},
-    gulp.src(
-      ['src/**/*.html', 'src/assets/**/*.*'],
-      {base: 'src'})
-      .pipe(gulp.dest('dist/'))
-  )
+gulp.task('copy', () => {
+  return gulp.src([
+    'src/index.html',
+    'src/assets/**',
+  ], {base: 'src'})
+    .pipe(gulp.dest(DIST));
 });
 
 // update all file names with md5-suffixed ones
@@ -146,21 +137,29 @@ gulp.task('rev', function () {
   )
 });
 
-// clean up unused generated files
-gulp.task('clean', function() {
-  return del.sync('dist');
-});
-
-gulp.task('browserSync-prod', function() {
-  browserSync.init({
-    server: {
-      baseDir: "./dist/"
-    }
-  })
+gulp.task('minifyhtml', () => {
+  let stream = gulp.src('src/pages/*.html', {base: 'src'})
+    .pipe(assetsInjector.inject({
+      link(html, asset) {
+        return '/' + path.relative('src', asset);
+      },
+      filter(html, asset) {
+        return path.basename(html, path.extname(html)) === path.basename(asset, path.extname(asset));
+      },
+    }));
+  if (isProd) stream = stream
+    .pipe(minifyHtml({
+      removeComments: true,
+      collapseWhitespace: true,
+      conservativeCollapse: true,
+      removeAttributeQuotes: true,
+    }));
+  return stream
+    .pipe(gulp.dest(DIST));
 });
 
 // Watch Files For Changes
-gulp.task('watch-prod', ['browserSync-prod'], function() {
+gulp.task('watch-prod', ['browserSync-prod'], function () {
   gulp.watch(['src/js/*.js']).on('change', browserSync.reload);
   gulp.watch(["src/**/*.html"]).on('change', browserSync.reload);
   gulp.watch('src/scss/*.scss', ['sass']);
@@ -169,7 +168,7 @@ gulp.task('watch-prod', ['browserSync-prod'], function() {
 
 gulp.task('prod', function (callback) {
   runSequence(
-    ['clean', 'uglify', 'sass-prod','images-prod', 'copy'],'rev',
+    'clean', ['minifyhtml', 'jshint', 'sass', 'images', 'copy'], 'rev',
     callback
   )
 });
---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+/**
+ * Created by kgd on 27/02/2017.
+ */
+
+console.log('ok');
